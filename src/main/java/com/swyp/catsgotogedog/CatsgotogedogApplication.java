5b77d9bcc41db3c423c174967e2ac7f702c2430d
spacedivver <142153611+spacedivver@users.noreply.github.com>
2025-07-05 15:58:09 +0900

    first commit

---
+++ src/main/java/com/swyp/catsgotogedog/CatsgotogedogApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.catsgotogedog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsgotogedogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsgotogedogApplication.class, args);
+	}
+
+}
