a1abb5971b553bb3470a193b6e13243f66630d30
yhs99 <khb113322@naver.com>
2025-07-15 18:37:40 +0900

    milvus 서비스 테스트코드 작성
    milvus 서비스 테스트코드 작성
    소셜 로그인 테스트를 위한 유저, 리프레쉬 토큰 테이블 생성 sql문 작성

---
+++ src/test/java/com/swyp/catsgotogedog/milvus/service/MilvusServiceTest.java
@@ -0,0 +1,53 @@
+package com.swyp.catsgotogedog.milvus.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import io.milvus.client.MilvusClient;
+import io.milvus.param.collection.LoadCollectionParam;
+
+@ExtendWith(MockitoExtension.class)
+public class MilvusServiceTest {
+
+	@Mock
+	private MilvusClient milvusClient;
+
+	@InjectMocks
+	private MilvusService milvusService;
+
+	private static final String TEST_COLLECTION_NAME = "catsgotogedog_test_collection";
+
+	@BeforeEach
+	void setUp() throws Exception {
+		ReflectionTestUtils.setField(milvusService, "collectionName", TEST_COLLECTION_NAME);
+	}
+
+	@Test
+	@DisplayName("컬렉션 로드 테스트 (성공)")
+	void loadCollection_success() {
+		assertDoesNotThrow(() -> milvusService.loadCollection());
+
+		verify(milvusClient, times(1)).loadCollection(any(LoadCollectionParam.class));
+	}
+
+	@Test
+	@DisplayName("컬렉션 로드 테스트 (실패)")
+	void loadCollection_Fail() {
+		doThrow(new RuntimeException("Milvus client load 실패")).when(milvusClient).loadCollection(any(LoadCollectionParam.class));
+
+		RuntimeException thrown = assertThrows(RuntimeException.class, () -> milvusService.loadCollection());
+
+		assert(thrown.getMessage().equals("Milvus user Creation Failed"));
+
+		verify(milvusClient, times(1)).loadCollection(any(LoadCollectionParam.class));
+	}
+}
