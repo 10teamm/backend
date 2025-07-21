c36fdd5c6b666e0054ecd78bcae2c0eb9473a503
yhs99 <khb113322@naver.com>
2025-07-21 18:55:38 +0900

    CORS 설정, User image_filename not null 제약해제

--- src/main/java/com/swyp/catsgotogedog/User/controller/UserController.java
+++ src/main/java/com/swyp/catsgotogedog/User/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements UserControllerSwagger{
 
     private final JwtTokenUtil jwt;
     private final RefreshTokenService rtService;
--- src/main/java/com/swyp/catsgotogedog/common/security/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/swyp/catsgotogedog/common/security/handler/OAuth2LoginSuccessHandler.java
@@ -1,5 +1,7 @@
 package com.swyp.catsgotogedog.common.security.handler;
 
+import java.io.IOException;
+
 import com.swyp.catsgotogedog.User.domain.entity.User;
 import com.swyp.catsgotogedog.User.repository.UserRepository;
 import com.swyp.catsgotogedog.User.service.RefreshTokenService;
@@ -8,25 +10,29 @@ import com.swyp.catsgotogedog.common.util.JwtTokenUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @RequiredArgsConstructor
-public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler
+	implements AuthenticationSuccessHandler {
 
     private final JwtTokenUtil jwt;
     private final RefreshTokenService rtService;
     private final UserRepository userRepo;
 
+    @Value("${frontend.base.url}")
+    private String frontend_base_url;
+
     @Override
     public void onAuthenticationSuccess(
-            HttpServletRequest request, HttpServletResponse response, Authentication auth) {
-
-//        String principal = auth.getName();
-//        User user = userRepo.findByUserId(principal).orElseThrow();
+            HttpServletRequest request, HttpServletResponse response, Authentication auth) throws IOException {
 
         PrincipalDetails pd = (PrincipalDetails) auth.getPrincipal();
         String providerId = pd.getProviderId();
@@ -39,8 +45,11 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
         rtService.save(user, refresh, jwt.getRefreshTokenExpiry());
 
-        response.setHeader("Authorization",   "Bearer " + access);
-        response.setHeader("X-Refresh-Token", refresh);
-        response.setStatus(HttpServletResponse.SC_OK);
+        String targetUrl = UriComponentsBuilder.fromUriString(frontend_base_url)
+            .queryParam("accessToken", access)
+            .queryParam("refreshToken", refresh)
+            .build()
+            .toUriString();
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
