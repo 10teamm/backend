9b845507610a32df6cf862176b656552b48bef90
jhhwang <5832120@naver.com>
2025-08-01 18:03:19 +0900

    반려동물 정보 API 추가

---
+++ src/main/resources/db/migration/mysql/V4__insert_pet_size_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `catsgotogedog`.`pet_size` (`size`, `size_tooltip`) VALUES ('소형', '소형견: 성견 된 몸무게가 대략 10kg 미만 (성견: 생후 2년 이상)');
+INSERT INTO `catsgotogedog`.`pet_size` (`size`, `size_tooltip`) VALUES ('중형', '중형견: 성견 된 몸무게가 대략 10~25kg 미만');
+INSERT INTO `catsgotogedog`.`pet_size` (`size`, `size_tooltip`) VALUES ('대형', '대형견: 성견 된 몸무게가 대략 25kg 이상');
