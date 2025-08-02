bb84efe8c996e9209f4485b2865f76f0a986fd1e
yhs99 <khb113322@naver.com>
2025-08-02 17:52:52 +0900

    user pets builder 초기화 수정
    user pets builder 초기화 수정

--- src/main/java/com/swyp/catsgotogedog/User/domain/entity/User.java
+++ src/main/java/com/swyp/catsgotogedog/User/domain/entity/User.java
@@ -30,8 +30,4 @@ public class User extends BaseTimeEntity {
     private String imageFilename;
     private String imageUrl;
     private Boolean isActive;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
-    @Builder.Default
-    private final List<Pet> pets = new ArrayList<>();
 }
