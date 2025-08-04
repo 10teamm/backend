ad68e4cad84b7495d35b439758a55af121d571e5
yhs99 <khb113322@naver.com>
2025-08-04 22:28:26 +0900

    feat/지역 코드 조회 API
    지역 코드 조회 API 개발

--- src/main/java/com/swyp/catsgotogedog/content/repository/RegionCodeRepository.java
+++ src/main/java/com/swyp/catsgotogedog/content/repository/RegionCodeRepository.java
@@ -4,15 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.swyp.catsgotogedog.content.domain.entity.RegionCode;
 
 public interface RegionCodeRepository extends JpaRepository<RegionCode, Integer> {
-	Optional<RegionCode> findBySidoCodeAndSigunguCode(int sidoCode, int sigunguCode);
-	Optional<RegionCode> findBySidoCodeAndSigunguCodeIsNull(int sidoCode);
-	List<RegionCode> findBySidoCode(int sidoCode);
-  
   RegionCode findBySidoCodeAndRegionLevel(int sidoCode, int regionLevel);
 
   RegionCode findByParentCodeAndSigunguCodeAndRegionLevel(int parentCode, int sigunguCode, int regionLevel);
+
+	List<RegionCode> findByRegionLevel(int regionLevel);
+
+	Optional<RegionCode> findByParentCodeAndSigunguCode(int regionId, Integer sigunguCode);
+
+	List<RegionCode> findByParentCode(int regionId);
 }
--- src/main/java/com/swyp/catsgotogedog/global/exception/ErrorCode.java
+++ src/main/java/com/swyp/catsgotogedog/global/exception/ErrorCode.java
@@ -2,6 +2,8 @@ package com.swyp.catsgotogedog.global.exception;
 
 import org.springframework.http.HttpStatus;
 
+import com.google.api.Http;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +24,8 @@ public enum ErrorCode {
         REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 리뷰입니다."),
         RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "리소스를 찾을 수 없습니다."),
         REVIEW_IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 리뷰 이미지입니다."),
+        SIGUNGU_CODE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 지역을 찾을 수 없습니다."),
+        SIDO_CODE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 시/도를 찾을 수 없습니다."),
 
         // 405 Method not allowed
         METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED.value(), "허용되지 않은 HTTP 메소드입니다."),
@@ -30,6 +34,7 @@ public enum ErrorCode {
         MESSAGE_NOT_READABLE(HttpStatus.BAD_REQUEST.value(), "요청 본문 형식이 올바르지 않습니다."),
         ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST.value(), "유효성 검사에 실패했습니다."),
         MISSING_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST.value(), "필수 파라미터가 누락되었습니다."),
+        SIGUNGU_NEEDS_WITH_SIDO_CODE(HttpStatus.BAD_REQUEST.value(), "시/군/구  코드는 반드시 시/도 코드와 함께 요청해야 합니다."),
 
         // 500 Internal Server Error
         INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 내부 오류가 발생했습니다."),
