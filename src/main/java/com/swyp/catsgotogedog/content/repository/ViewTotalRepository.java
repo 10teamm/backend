aa53030bebde7373d3f6bbf6f22d519e96614c02
wooodev <142153611+wooodev@users.noreply.github.com>
2025-08-06 23:27:42 +0900

    fix: 조회수 갱신 오류 해결 #54
    Modifying, 트랜잭션 처리

--- src/main/java/com/swyp/catsgotogedog/content/repository/ViewTotalRepository.java
+++ src/main/java/com/swyp/catsgotogedog/content/repository/ViewTotalRepository.java
@@ -1,10 +1,14 @@
 package com.swyp.catsgotogedog.content.repository;
 
 import com.swyp.catsgotogedog.content.domain.entity.ViewTotal;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ViewTotalRepository extends JpaRepository<ViewTotal, Integer> {
+    @Modifying
+    @Transactional
     @Query(value = """
         INSERT INTO view_total (content_id, total_view, updated_at)
         VALUES (:contentId, 1, NOW())
