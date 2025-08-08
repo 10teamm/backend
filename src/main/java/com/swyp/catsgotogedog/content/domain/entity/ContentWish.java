92f7f8df6cd433f1697773b01bf0067d733cbaeb
wooodev <142153611+wooodev@users.noreply.github.com>
2025-08-08 23:23:38 +0900

    fix: 기본 생성자 어노테이션 추가 #86

--- src/main/java/com/swyp/catsgotogedog/content/domain/entity/ContentWish.java
+++ src/main/java/com/swyp/catsgotogedog/content/domain/entity/ContentWish.java
@@ -7,12 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Entity
 @Getter
 @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ContentWish {
 
     @Id
