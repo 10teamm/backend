655e8c50395beae4d8b89fee195b97f38f61604d
yhs99 <khb113322@naver.com>
2025-08-10 17:03:49 +0900

    refactor/ 최근 본 장소 저장 로직 보완
    최근 본 장소 저장 로직 보완

--- src/main/java/com/swyp/catsgotogedog/mypage/domain/entity/LastViewHistoryId.java
+++ src/main/java/com/swyp/catsgotogedog/mypage/domain/entity/LastViewHistoryId.java
@@ -8,9 +8,11 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Embeddable
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
